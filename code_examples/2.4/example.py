45bb319432985fc101beb4109d8347f3f4bc9b5f
beary <javier.guerra1001@gmail.com>
2026-04-23 09:12:00 -0700

    added code example for lesson 2.4

---
+++ code_examples/2.4/example.py
@@ -0,0 +1,17 @@
+message = "hello I like pickles on my sandwich"
+print(message.capitalize())
+print(message.title())
+print(message.swapcase())
+print(message.find("pickles"))
+print(message.count("e"))
+print(len(message))
+user_id = "1"
+print(user_id.zfill(5))
+intro = "WELCOME!"
+print(intro.center(30, "*"))
+csv_details = "John,25,engineering"
+print(csv_details.split(","))
+date_joined = "-".join(["2025", "11", "01"])
+print(date_joined)
+package_id = "1234567"
+print(package_id.isdigit())
