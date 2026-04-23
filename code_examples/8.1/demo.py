375b4f55d69f9584f8ce07297168c769fe14ddea
beary <javier.guerra1001@gmail.com>
2026-04-23 11:46:03 -0700

    docs: added instructions in comments to create python venv, activate, and use pip to install dependencies from requirements.txt

--- code_examples/8.1/demo.py
+++ code_examples/8.1/demo.py
@@ -1,5 +1,8 @@
 # remember requests is a 3rd party module, you will need to install it
 # create a python virtual environment and then install dependencies from requirements.txt file
+# how to create python virtual environment windows: `python -m venv venv` mac/linux: `python3 -m venv venv`
+# activate python virtual environment windows `:.\venv\Scripts\activate` mac/linux: `source venv/bin/activate`
+# install dependencies from requirements.txt `pip install -r requirements.txt`
 import requests
 response = requests.get("https://www.google.com")
 print(response.text)
